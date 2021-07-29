3bb1b9a84c694205f2933f8ad77739e9bbccc047
danger2123 <sellmysolenyc@gmail.com>
2021-07-28 23:31:55 -0400

    created renderReview function and refractor JS code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const nextBtn = document.getElementById("next-btn");
 const prevBtn = document.getElementById("prev-btn");
 const image = document.getElementById("image");
+const reviewname = document.getElementById("name");
 const job = document.getElementById("job");
 const review = document.getElementById("review");
 let reviewCounter = 0;
@@ -8,6 +9,7 @@ let reviewCounter = 0;
 const reviews = [
   {
     id: 1,
+    name: "Vin Diesel",
     job: "Back End Developer",
     img: "url('./images/vin-diesel.jpeg')",
     reviewText:
@@ -15,6 +17,7 @@ const reviews = [
   },
   {
     id: 2,
+    name: "Chris Rock",
     img: "url('./images/chris-rock.jpeg')",
     job: "Front End Developer",
     reviewText:
@@ -23,6 +26,7 @@ const reviews = [
   {
     id: 3,
     img: "url('./images/seth-rogen.jpeg')",
+    name: "Seth Rogan",
     job: "Designer",
     reviewText:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus neque ab vitae consectetur quidem molestiae, iusto placeat, eveniet sint sapiente iste ea nam accusantium, aspernatur natus! Odio nemo sunt voluptatum.Lorem ipsum dolor sit amet consectetur adipisicing elit.",
@@ -30,57 +34,51 @@ const reviews = [
   {
     id: 4,
     img: "url('./images/the-rock.jpeg')",
+    name: "The Rock",
     job: "The Boss",
     reviewText:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus neque ab vitae consectetur quidem molestiae, iusto placeat, eveniet sint sapiente iste ea nam accusantium, aspernatur natus! Odio nemo sunt voluptatum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus neque ab vitae consectetur quidem molestiae, iusto placeat, eveniet sint sapiente iste ea nam accusantium.",
   },
+  {
+    id: 5,
+    img: "url('./images/me.jpg')",
+    name: "Chris Lutch",
+    job: "Full Stack Developer",
+    reviewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus neque ab vitae consectetur quidem molestiae, iusto placeat, eveniet sint sapiente iste ea nam accusantium, aspernatur natus! Odio nemo sunt voluptatum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus neque ab vitae consectetur quidem molestiae, iusto placeat, eveniet sint sapiente iste ea nam accusantium.Accusamus neque ab vitae consectetur quidem molestiae, iusto placeat, eveniet sint sapiente iste ea nam accusantium.",
+  },
 ];
 
 if (reviewCounter === 0) {
-  image.setAttribute(
-    `style`,
-    `background-image: ${reviews[reviewCounter].img}`
-  );
-  job.textContent = reviews[reviewCounter].job;
-  review.textContent = reviews[reviewCounter].reviewText;
+  renderReview();
 }
 
 nextBtn.addEventListener("click", function () {
   if (reviewCounter === reviews.length - 1) {
     reviewCounter = 0;
-    image.setAttribute(
-      `style`,
-      `background-image: ${reviews[reviewCounter].img}`
-    );
-    job.textContent = reviews[reviewCounter].job;
-    review.textContent = reviews[reviewCounter].reviewText;
+    renderReview();
   } else if (reviewCounter < reviews.length) {
     reviewCounter++;
-    image.setAttribute(
-      `style`,
-      `background-image: ${reviews[reviewCounter].img}`
-    );
-    job.textContent = reviews[reviewCounter].job;
-    review.textContent = reviews[reviewCounter].reviewText;
+    renderReview();
   }
 });
 
 prevBtn.addEventListener("click", function () {
   if (reviewCounter === 0) {
     reviewCounter = reviews.length - 1;
-    image.setAttribute(
-      `style`,
-      `background-image: ${reviews[reviewCounter].img}`
-    );
-    job.textContent = reviews[reviewCounter].job;
-    review.textContent = reviews[reviewCounter].reviewText;
+    renderReview();
   } else if (reviewCounter > 0) {
     reviewCounter--;
-    image.setAttribute(
-      `style`,
-      `background-image: ${reviews[reviewCounter].img}`
-    );
-    job.textContent = reviews[reviewCounter].job;
-    review.textContent = reviews[reviewCounter].reviewText;
+    renderReview();
   }
 });
+
+function renderReview() {
+  image.setAttribute(
+    `style`,
+    `background-image: ${reviews[reviewCounter].img}`
+  );
+  reviewname.textContent = reviews[reviewCounter].name;
+  job.textContent = reviews[reviewCounter].job;
+  review.textContent = reviews[reviewCounter].reviewText;
+}
